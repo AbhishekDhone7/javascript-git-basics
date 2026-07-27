ddc5e6e203b55b345b2163e1fb5ffc376990217b
Julekha Chaos <julekhachaus@gmail.com>
2026-07-27 22:22:59 +0530

    newfile plus one change in array.js

--- Array.js
+++ Array.js
@@ -1,7 +1,7 @@
 //? Array Methods
 
 {
-  let array = [1, 2, 3, 4, 5, 6];
+  let array = [1, 2, 3, 4, 5, 8];
   let opration = array.push(7);
   console.log(opration);
   console.log(array);
---
+++ first.js
@@ -0,0 +1,6 @@
+{
+  let array = [1, 2, 3, 4, 5, 8];
+  let opration = array.push(7);
+  console.log(opration);
+  console.log(array);
+}
